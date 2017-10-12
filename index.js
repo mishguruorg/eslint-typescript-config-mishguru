326f1b745cff5fab72488a2ffae0542b57714202
Jin Jun(MAC) <cojj.dev@gmail.com>
2017-10-12 16:54:13 +1300

    downgrade babel-eslint to 7.2.3 - 8 doesn't work with eslint-plugin-flowtype

--- index.js
+++ index.js
@@ -22,10 +22,6 @@ module.exports = {
 
     // Enforce consistent spacing inside braces
     // http://eslint.org/docs/rules/object-curly-spacing
-    'object-curly-spacing': [2, 'always'],
-
-    // Marks Flow type identifiers as defined.
-    // https://github.com/gajus/eslint-plugin-flowtype#eslint-plugin-flowtype-rules-define-flow-type
-    'flowtype/define-flow-type': 1
+    'object-curly-spacing': [2, 'always']
   }
 }
