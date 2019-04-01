de34b6c978b52da5ef1af03b50eec1c0ad1ecb7d
George Czabania <george@mish.guru>
2019-04-02 04:22:54 +1300

    Update deps and use ava

--- test/validate.spec.js
+++ test/validate.spec.js
@@ -1,8 +1,9 @@
+const test = require('ava')
+
 const eslint = require('eslint')
-const test = require('tape')
 const path = require('path')
 
-test('load config in eslint to validate all rule syntax is correct', function (t) {
+test('load config in eslint to validate all rule syntax is correct', (t) => {
   const CLIEngine = eslint.CLIEngine
 
   const cli = new CLIEngine({
@@ -13,15 +14,12 @@ test('load config in eslint to validate all rule syntax is correct', function (t
 
   const report = cli.executeOnFiles([
     path.join(__dirname, '../sample'),
-    path.join(__dirname, '../index.js'),
+    path.join(__dirname, '../index.js')
   ])
 
-  for (const file of report.results) {
-    if (file.errorCount > 0) {
-      console.log(file.messages)
-    }
+  if (report.errorCount > 0) {
+    console.log(report.results[0].messages)
   }
 
-  t.ok(report.errorCount === 0)
-  t.end()
+  t.is(report.errorCount, 0)
 })
